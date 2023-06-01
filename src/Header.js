ae8a99b15ee45e1e2406fd2c95ac98c0719c14ce
Narmadha <vnarmadha286@gmail.com>
2023-06-01 19:30:55 +0530

    first

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <header>
+        <h1>{props.title}</h1>
+    </header>
+  )
+}
+Header.defaultProps={
+  title:"To do List"
+}
+export default Header
